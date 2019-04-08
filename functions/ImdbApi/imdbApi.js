1c11f621764f1776e3d7893af7f99a31717c7c99
Rahul Potluri <p.s.rahul1990@gmail.com>
2019-04-08 14:41:40 -0500

    Update imdbApi.js

--- functions/ImdbApi/imdbApi.js
+++ functions/ImdbApi/imdbApi.js
@@ -1,14 +1,52 @@
-const request = require('request');
-const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=d20bef07';
+const rq = require('request-promise');
 
-const movieTitle = 'furious+7';
+
+//const apiUrl = 'http://www.omdbapi.com/?apikey=d20bef07' + '&t=' + 'furious+7';
+
+var options = {
+    method: 'GET',
+    uri: 'http://www.omdbapi.com/',
+    qs: {
+        apikey: 'd20bef07',        
+    },    
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    json: true
+};
+
+function getMovieDetails() {
+    const movieTitle = 'furious+7';
+    options.qs.t = movieTitle;
+    
+    console.log(options);
+
+    rq(options)
+        .then( body => {
+
+                console.log(body);
+                return body;
+                        
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+}
+
+// function getMovieDetails(agent) {
+//     return new Promise((resolve, reject) => { 
+//         request(apiUrl, function (error, response, body) {
+//             if(!error && response.statusCode === 200) {
+//                 console.log(body) // Print the google web page.
+//                 resolve(body);
+//             } else {
+//                 reject(error);
+//             }
+//         });
+//     });
+// }
+
+
 
 exports.movieDetails = getMovieDetails;
-
-function getMovieDetails(agent) {
-return request(apiUrl, function (error, response, body) {
-    if(!error && response.statusCode === 200) {
-        console.log(body) // Print the google web page.
-     }
-});
-}
