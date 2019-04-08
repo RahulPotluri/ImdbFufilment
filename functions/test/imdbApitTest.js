b15a2a502216edfcf3d7845d3ee494a29086b5e1
Rahul Potluri <p.s.rahul1990@gmail.com>
2019-04-08 14:41:23 -0500

    Update imdbApitTest.js

--- functions/test/imdbApitTest.js
+++ functions/test/imdbApitTest.js
@@ -1,11 +1,14 @@
 const assert = require('assert');
 var mocha = require('mocha');
-var imdbApi = require('./functions/ImdbApi/imdbApi');
-var it = mocha.it;
-var describe = mocha.describe;
+var imdbApi = require('../ImdbApi/imdbApi');
+// var it = mocha.it;
+// var describe = mocha.describe;
 
-describe("imdb response api", function() {
-    it('get response from body', function(){
-        assert.equal('response', imdbApi.movieDetails)
+describe('imdb response api', function() {
+    
+    it('get response from body', function() {
+          assert.equal('response', imdbApi.movieDetails());
+        
+        
     })
 });
